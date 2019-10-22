1f29cbf9656db379e719aef797e34d0bdacebee5
Kerri-Ann <kaybee1jam@yahoo.com>
2019-10-22 10:25:25 -0400

    added homepage and styles

---
+++ anywhere-fitness/src/components/Landing/Home.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+
+const Home = () => {
+    return (
+        <>
+            <section className="main-hero">
+                <div className="hero-image">
+                    <Container>
+                        <div className="main-heading-block">
+                            <h2>Take your fitness anywhere</h2>
+                            <h3>Empowering you to take control of your health</h3>
+                        </div>
+                    </Container>
+                </div>
+            </section>
+            <main className="front-page-sections">
+                <Container>
+                    <div className="section-content">
+                        <div className="img-container">
+                            <img src="images/DesertVan.jpg" alt="Van driving through the desert" />
+                        </div>
+                        <div className="content-card primary-card">
+                            <div className="card-text">
+                                <h4>Take the stress away from planning</h4>
+                                <p>Whatever it is that you're planning for, it doesn't have to be boring or difficult. Have fun doing it with ease.</p>
+                            </div>
+                        </div>
+                    </div>
+                </Container>
+            </main>
+            <section className="front-page-sections secondary-section">
+                <Container>
+                    <div className="section-content">
+                        <div className="content-card secondary-card">
+                            <div className="card-text">
+                                <h4>Get the whole crew involved</h4>
+                                <p>Easily include everyone in your plans by sharing your ideas. Get inspired by others and share the excitement.</p>
+                            </div>
+                        </div>
+                        <div className="img-container">
+                            <img src="images/getaway.jpg" alt="Friends and family gathering by the pool" />
+                        </div>
+                    </div>
+                </Container>
+            </section>
+            <section className="front-page-sections features">
+                <Container>
+                    <div className="features-parent">
+                        <div className="feature-block">
+                            <i className="fas fa-plane-departure"></i>
+                            <h3>Vacation Planning</h3>
+                            <p>Plan that trip for an exotic adventure</p>
+                        </div>
+                        <div className="feature-block">
+                            <i className="fas fa-home"></i>
+                            <h3>Goals Planning</h3>
+                            <p>Set long term or short term goals with ease</p>
+                        </div>
+                        <div className="feature-block">
+                            <i className="fas fa-users"></i>
+                            <h3>Share Your Plans</h3>
+                            <p>Share your plans with confidence</p>
+                        </div>
+                    </div>
+                </Container>
+            </section>
+        </>
+    )
+}
+
+export default Home;
