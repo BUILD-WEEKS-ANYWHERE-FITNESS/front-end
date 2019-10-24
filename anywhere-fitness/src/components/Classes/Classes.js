068986a5a68916a3ef096729560accc1368c3c0d
SilverMaiden <h.specials@gmail.com>
2019-10-24 13:41:39 -0700

    Added search classes functionality

--- anywhere-fitness/src/components/Classes/Classes.js
+++ anywhere-fitness/src/components/Classes/Classes.js
@@ -1,36 +1,104 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Input } from 'reactstrap';
-import { getData } from '../../actions'
+import ClassesCard from "./ClassesCard";
+import axios from "axios";
 
 
 const Classes = (props) => {
+    //Fake data, for now
+    const [search, setSearch] = useState({
+    searchTerm: "",
+    duration: "",
+    type: "",
+    intensity: "",
+    })
+    const [classes,setClasses] = useState([
+        {
+        name: 'Intro to Yoga',
+        duration: '30 mins',
+        type: "yoga",
+        intensity: 'beginner'
+        },
+        {
+          name: 'Intense Pilates',
+          duration: '60 mins',
+          type: "pilates",
+          intensity: 'intermediate'
+        },
+        {
+          name: 'Masterclass: Yoga',
+          duration: '45 mins',
+          type: "yoga",
+          intensity: 'advanced'
+        },
+        {
+          name: 'Intro to Pilates',
+          duration: '30 mins',
+          type: "pilates",
+          intensity: 'beginner'
+        }
+      ])
 
-  // useEffect(() => {
-  //   props.getData()
-  //   console.log(props,`classList`)
-  // }, [])
+      const [searchResults, setSearchResults] = useState([]);
 
+      useEffect(() => {
+          const resultSearch = classes.filter(classObj =>
+                             classObj.name.toLowerCase().includes(search.searchTerm.toLowerCase())
+                                        );
+         const resultDuration = resultSearch.filter(classObj =>
+                                              classObj.duration.includes(search.duration)
+                                              )
+
+         const resultType = resultDuration.filter(classObj =>
+                                                 classObj.type.toLowerCase().includes(search.type.toLowerCase())
+                                                 )
+         const resultIntensity = resultType.filter(classObj =>
+                                                 classObj.intensity.toLowerCase().includes(search.intensity.toLowerCase())
+                                                 )
+
+                setSearchResults(resultIntensity);
+
+      }, [search]);
+
+      //Controlled Inputs
+      //
+      const changeHandler = (e) => {
+          setSearch({...search, [e.target.name]: e.target.value})
+      }
   return (
     <div className="card-container">
     <div className="classes">
         <h1> Search Classes </h1>
         <div className="class-search-card">
-            <Input className="class-search-bar" type="search" name="search" id="search" placeholder="Search" />
-            <Input className="class-search-drops" type="select" name="select" id="select">
-              <option>Duration</option>
-              <option>30 mins</option>
-              <option>45 mins</option>
-              <option>60 mins</option>
+            <Input className="class-search-bar" type="search" name="searchTerm" id="searchTerm" placeholder="Search" onChange={changeHandler}/>
+            <Input className="class-search-drops" type="select" name="duration" id="duration" onChange={changeHandler}>
+              <option value="">Duration</option>
+              <option value="30 mins">30 mins</option>
+              <option value="45 mins">45 mins</option>
+              <option value="60 mins">60 mins</option>
         </Input>
-        <Input className="class-search-drops" type="select" name="select" id="select">
-              <option>Type</option>
-              <option>Yoga</option>
-              <option>Pilates</option>
+        <Input className="class-search-drops" type="select" name="type" id="type" onChange={changeHandler}>
+              <option value="">Type</option>
+              <option value="Yoga">Yoga</option>
+              <option value="Pilates">Pilates</option>
+        </Input>
+        <Input className="class-search-drops" type="select" name="intensity" id="intensity" onChange={changeHandler}>
+              <option value="">Intensity</option>
+              <option value="Beginner">Beginner</option>
+              <option value="Intermediate">Intermediate</option>
+              <option value="Advances">Advanced</option>
         </Input>
 
-            </div>
-            </div>
         </div>
+        <div className="class-cards-container">
+            {searchResults.map(singleClass => (
+                    <ClassesCard singleClass={singleClass} />
+                ))}
+        </div>
+
+            </div>
+
+    </div>
 
   );
 }
